2b15bb584fb38bc31f95afaed66208465f36c7e4
Dylan Hyrkas <dylhyrkas@gmail.com>
2025-04-13 13:18:10 -0500

    changes

--- script.js
+++ script.js
@@ -1,4 +1,36 @@
-console.log("SUS");
+const sounds = {
+  click: document.getElementById("clickSound"),
+  ribbit: new Audio("assets/ribbit.mp3"),
+};
+
 function SendDetails() {
-    alert("Details Sent ¯\_(ツ)_/¯");
+  sounds.click.play();
+  const email = document.getElementById("email").value;
+  setTimeout(() => {
+    alert(
+      `Thanks ${
+        email || "valued customer"
+      }!\nYour data has been securely sold to our partners.`
+    );
+    sounds.ribbit.play();
+
+    document.querySelectorAll("button").forEach((btn) => {
+      btn.style.background = "linear-gradient(to bottom, #ff0000, #cc0000)";
+      btn.textContent = "CLICK AGAIN FOR MORE SECURITY";
+    });
+  }, 800);
 }
+
+function Download() {
+  sounds.click.play();
+  setTimeout(() => {
+    alert(
+      "Error: Your device has been flagged by the CIA.\nPlease enter credit card details to continue."
+    );
+    window.location.href = "https://youtu.be/dQw4w9WgXcQ";
+  }, 500);
+}
+
+setInterval(() => {
+  sounds.ribbit.play();
+}, 30000);
